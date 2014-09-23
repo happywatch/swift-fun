cd42a9d72aaa01fe7aa3df2ba818e49a940dd834
Ben Morrow <ben@benmorrow.info>
2014-09-23 09:34:31 -0700

    Adding simple algorithms

--- README.md
+++ README.md
@@ -6,11 +6,12 @@ Learn Swift syntax with a playground. Playgrounds are a new way to test out code
 Topics covered:
 
 * Declaring variables and constants
-* Arrays and dictionaries
+* Arrays, dictionaries, and optionals
 * Loops
 * Functions and tuples
+* Simple algorithms using "if let" and optionals
 * UIKit Views 
 * Optional parameters in functions
 * Generics and inout
-* A function as a parameter
+* Passing a function as a parameter
 * High order functions for Functional Programming
--- swift-fun.playground/section-1.swift
+++ swift-fun.playground/section-1.swift
@@ -18,7 +18,7 @@ str = "Howdy"
 
 
 /***
-Arrays and Dictionaries
+Arrays, Dictionaries, and Optionals
 ***/
 
 var array = ["one", "two"]
@@ -40,12 +40,6 @@ three = 3
 three!
 
 let jillsAgeTimesTwo = dictionary["Jill"]! * 2
-
-if let bobsAge = dictionary["Bob"] {
-    print(bobsAge)
-} else {
-    print("Bob's not in the dictionary")
-}
 
 
 
@@ -70,39 +64,111 @@ for x in 1..<10 {
 Functions and Tuples
 ***/
 
+// Defining a function
+
 func printHello() {
     println("Hello")
 }
 
+// Calling the function
+
 printHello()
 
 
+// Parameters
 
 func printArea(height: Int, width: Int){
     let area = height * width
     println("The area is \(area)")
 }
 
+// Calling the function with parameters
+
 printArea(10, 12)
 
 
+// Named parameters and a return type
 
-func getArea(height: Int, width: Int) -> Int{
+func calculateArea(#height: Int, #width: Int) -> Int{
     return height * width
 }
 
-println("The area is \(getArea(10, 12))")
+// Making use of the returned value
+
+println("The area is \(calculateArea(height: 10, width: 12))") // Notice that the parameter names are included in the function call
 
 
 
 
 var aTuple = ("Bill", 42, UIColor.blueColor(), CGPointMake(20, 30))
 
+// Naming the tuple components in a function return
+
 func willReturnTuple() -> (String, Int, UIColor, CGPoint) {
     return aTuple
 }
 
+// Accessing the tuple component by calling the function
+
 willReturnTuple().1
+
+// Naming the function parameter and the return values
+
+func willReturnNamedTuple(whichTuple thisTuple: (String, Int, UIColor, CGPoint)) -> (name: String, age: Int, color: UIColor, location: CGPoint) {
+    return thisTuple
+}
+
+// Accessing the tuple component by calling the function
+
+willReturnNamedTuple(whichTuple: aTuple).age
+
+
+
+
+
+/***
+Simple algorithms
+Using "if let" and optionals
+***/
+
+
+// See if two numbers are divisible
+
+func testDivisibility(a: Int, by b: Int) -> Int?{
+    if a % b == 0 {
+        return nil
+    } else {
+        return a % b
+    }
+}
+
+if let remainder = testDivisibility(3, by: 2) {
+    println("The remainder is \(remainder)")
+} else {
+    println("Divisible")
+}
+
+
+
+
+// Retreive a person's age by searching for their name
+
+func search(#who: String) -> Int? {
+    let people = ["Bill": 26,"Bob": 42,"Ben": 37]
+    for (name, age) in people {
+        if name == who {
+            return age
+        }
+    }
+    return nil
+}
+
+if let age = search(who: "Doc") {
+    println("This person's age is \(age)")
+} else {
+    println("This person wasn't found")
+}
+
 
 
 
@@ -179,7 +245,7 @@ swap(&c, &d)
 
 
 /***
-Function as a parameter
+Passing a function as a parameter
 ***/
 
 func sum(a: Int, b: Int) -> Int{
@@ -211,6 +277,8 @@ levelThree
 
 var levelFour = sumOfFunction(3, 4, {$0 * $0})
 levelFour
+
+
 
 
 /***
